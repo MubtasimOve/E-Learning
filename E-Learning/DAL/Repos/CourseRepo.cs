6948f9f1d089be2aa9e8b7220aa46d2b0dd706fd
Sazzadu42644 <20-42644-1@student.aiub.edu>
2023-08-10 20:34:06 +0600

    Create IRepo,Repo,TeacherRepo& CourseREpo

---
+++ E-Learning/DAL/Repos/CourseRepo.cs
@@ -0,0 +1,43 @@
+﻿using DAL.EF.Model;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class CourseRepo : Repo, IRepo<Course, int, bool>
+    {
+        public bool Create(Course obj)
+        {
+            db.Courses.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var db_obj = Get(id);
+            db.Courses.Remove(db_obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Course> Get()
+        {
+            return db.Courses.ToList();
+        }
+
+        public Course Get(int id)
+        {
+            return db.Courses.Find(id);
+        }
+
+        public bool Update(Course obj)
+        {
+            var db_obj = Get(obj.Id);
+            db.Entry(db_obj).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
---
+++ E-Learning/DAL/Repos/TeacherRepo.cs
@@ -0,0 +1,43 @@
+﻿using DAL.EF.Model;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class TeacherRepo : Repo, IRepo<Teacher, int, bool>
+    {
+        public bool Create(Teacher obj)
+        {
+            db.Teachers.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var db_obj = Get(id);
+            db.Teachers.Remove(db_obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Teacher> Get()
+        {
+            return db.Teachers.ToList();
+        }
+
+        public Teacher Get(int id)
+        {
+            return db.Teachers.Find(id);
+        }
+
+        public bool Update(Teacher obj)
+        {
+            var db_obj = Get(obj.Id);
+            db.Entry(db_obj).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
