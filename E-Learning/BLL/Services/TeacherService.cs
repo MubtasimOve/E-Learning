2b5c06c40691fd9b366080df2d971433766e752c
Sazzadu42644 <20-42644-1@student.aiub.edu>
2023-08-12 04:02:13 +0600

    Teacher crud api

---
+++ E-Learning/BLL/Services/TeacherService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class TeacherService
+    {
+        public static TeacherDTO Add(TeacherDTO obj)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<TeacherDTO, Teacher>();
+                cfg.CreateMap<Teacher, TeacherDTO>();
+            });
+            var mapper = new Mapper(config);
+            var conv = mapper.Map<Teacher>(obj);
+            var rs = DataAccessFactory.TeacherDataAccess().Create(conv);
+
+            return mapper.Map<TeacherDTO>(rs);
+
+        }
+        public static List<TeacherDTO> Get()
+        {
+            var data = DataAccessFactory.TeacherDataAccess().Get();
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Teacher, TeacherDTO>();
+            });
+            var mapper = new Mapper(config);
+            var cnvrt = mapper.Map<List<TeacherDTO>>(data);
+            return cnvrt;
+        }
+        public static bool Delete(int id)
+        {
+            return DataAccessFactory.TeacherDataAccess().Delete(id);
+        }
+        public static bool Update(TeacherDTO obj)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<TeacherDTO, Teacher>();
+            });
+            var mapper = new Mapper(config);
+            var conv = mapper.Map<Teacher>(obj);
+            var rs = DataAccessFactory.TeacherDataAccess().Update(conv);
+
+            return rs;
+        }
+        public static TeacherDTO Get(int id)
+        {
+            var data = DataAccessFactory.TeacherDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Teacher, TeacherDTO>();
+            });
+            var mapper = new Mapper(config);
+            var cnvrt = mapper.Map<TeacherDTO>(data);
+            return cnvrt;
+        }
+        public static List<TeacherDTO> GetByName(string name)
+        {
+            var data = (from p in DataAccessFactory.TeacherDataAccess().Get()
+                        where p.Name == name
+                        select p).ToList();
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Teacher, TeacherDTO>();
+            });
+            var mapper = new Mapper(config);
+            var cnvrt = mapper.Map<List<TeacherDTO>>(data);
+            return cnvrt;
+        }
+    }
+}
---
+++ E-Learning/E-Learning/Controllers/TeacherController.cs
@@ -0,0 +1,112 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace E_Learning.Controllers
+{
+    public class TeacherController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Teacher/add")]
+
+        public HttpResponseMessage Add(TeacherDTO obj)
+        {
+            try
+            {
+                var data = TeacherService.Add(obj);
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Created" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+        [HttpGet]
+        [Route("api/Teacher/all")]
+        public HttpResponseMessage All()
+        {
+            try
+            {
+                var data = TeacherService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+        [HttpGet]
+        [Route("api/Teacher/delete/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                var data = TeacherService.Delete(id);
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "deleted" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/Teacher/update")]
+        public HttpResponseMessage update(TeacherDTO obj)
+        {
+            try
+            {
+                var data = TeacherService.Update(obj);
+                return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "updated" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("api/Teacher/get/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                var data = TeacherService.Get(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("api/Teacher/name/{name}")]
+        public HttpResponseMessage GetByName(string name)
+        {
+            try
+            {
+                var data = false;
+                if (data == true)
+                {
+                    TeacherService.GetByName(name);
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Found" });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
